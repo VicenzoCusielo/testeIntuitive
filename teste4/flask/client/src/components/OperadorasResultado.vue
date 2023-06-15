eb6d3fa71a7ed6aac7a0c5632279f2cb4f04d83d
VicenzoCusielo <vicenzocusielo@outlook.com>
2023-06-15 16:26:33 -0300

    Update OperadorasResultado.vue

--- teste4/flask/client/src/components/OperadorasResultado.vue
+++ teste4/flask/client/src/components/OperadorasResultado.vue
@@ -3,25 +3,25 @@
     <div class="container mt-5"><ol>
         <div v-if="!operadoras.length">Nenhum resultado</div>
       <div v-for="operadora in operadoras" :key="operadora[0]"><li>
-        <h3>Registro ANS: {{operadora[1]}}</h3>
-        <h4>CNPJ: {{operadora[2]}}</h4>
-        <p>Razão Social: {{operadora[3]}}
-        Nome Fantasia: {{operadora[4]}} 
-        Modalidade: {{operadora[5] }} 
-        Logradouro: {{operadora[6] }} 
-        Número: {{operadora[7] }} 
-        Complemento: {{operadora[8] }} 
-        Bairro: {{operadora[9] }} 
-        Cidade: {{operadora[10] }} 
-        UF: {{operadora[11] }} 
-        CEP: {{operadora[12] }} 
-        DDD: {{operadora[13] }} 
-        Telefone: {{operadora[14] }}
-        Fax: {{operadora[15] }} 
-        Endereço de e-mail: {{operadora[16] }} 
-        Representante: {{operadora[17] }} 
-        Cargo do representante: {{operadora18 }} 
-        Data de registro: {{operadora[19] }} </p>
+        <h3>Registro ANS: {{operadora[0]}}</h3>
+        <h4>CNPJ: {{operadora[1]}}</h4>
+        <p>Razão Social: {{operadora[2]}}
+        Nome Fantasia: {{operadora[3]}} 
+        Modalidade: {{operadora[4] }} 
+        Logradouro: {{operadora[5] }} 
+        Número: {{operadora[6] }} 
+        Complemento: {{operadora[7] }} 
+        Bairro: {{operadora[8] }} 
+        Cidade: {{operadora[9] }} 
+        UF: {{operadora[10] }} 
+        CEP: {{operadora[11] }} 
+        DDD: {{operadora[12] }} 
+        Telefone: {{operadora[13] }}
+        Fax: {{operadora[14] }} 
+        Endereço de e-mail: {{operadora[15] }} 
+        Representante: {{operadora[16] }} 
+        Cargo do representante: {{operadora[17] }} 
+        Data de registro: {{operadora[18] }} </p>
       </li>
       </div>
     </ol>
